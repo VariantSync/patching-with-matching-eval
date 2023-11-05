cba934cd4516336a8f088e78e1b129b49b279886
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2023-11-05 14:51:33 +0100

    tests: add tests for handling of hunk starts

--- src/test/java/SpecialCasesTest.java
+++ src/test/java/SpecialCasesTest.java
@@ -2,7 +2,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.variantsync.evaluation.baseline.diff.DiffParser;
 import org.variantsync.evaluation.baseline.diff.components.FineDiff;
-import org.variantsync.evaluation.baseline.diff.filter.ILineFilter;
 import org.variantsync.evaluation.baseline.diff.splitting.DefaultContextProvider;
 import org.variantsync.evaluation.baseline.diff.splitting.DiffSplitter;
 import org.variantsync.evaluation.baseline.diff.splitting.IContextProvider;
@@ -13,21 +12,31 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
-public class EOFContexTest {
-    Path resourceDir = Paths.get("src", "test", "resources", "eof");
+public class SpecialCasesTest {
+    Path eofTests = Paths.get("src", "test", "resources", "eof");
+    Path entireFileTests = Paths.get("src", "test", "resources", "entire-file");
 
-    @Test
-    public void runComparison() throws IOException {
-        List<String> diffLines = Files.readAllLines(resourceDir.resolve("original.diff"));
-        IContextProvider contextProvider = new DefaultContextProvider(resourceDir, false);
+    public void runComparison(Path testDir) throws IOException {
+        List<String> diffLines = Files.readAllLines(testDir.resolve("original.diff"));
+        IContextProvider contextProvider = new DefaultContextProvider(testDir, false);
         FineDiff fineDiff = DiffSplitter.split(DiffParser.toOriginalDiff(diffLines), null, null, contextProvider);
 
-        List<String> expectedLines = Files.readAllLines(resourceDir.resolve("splits.diff"));
+        List<String> expectedLines = Files.readAllLines(testDir.resolve("splits.diff"));
         List<String> actualLines = fineDiff.toLines();
         for (int i = 0; i < expectedLines.size(); i++) {
             String expectedLine = expectedLines.get(i);
             String actualLine = actualLines.get(i);
             Assertions.assertEquals(expectedLine, actualLine, "Mismatch in line " + (i+1));
         }
+    }
+
+    @Test
+    public void eofTest() throws IOException {
+        runComparison(eofTests);
+    }
+
+    @Test
+    public void entireFileTest() throws IOException {
+        runComparison(entireFileTests);
     }
 }
