b19d35d7edda4cfd185bad625207b3c3a7b5d08f
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2024-02-15 18:33:08 +0100

    chore: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.9.0"
@@ -19,12 +18,15 @@ repositories {
 dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("org.variantsync.vevos:simulation:1.2.0")
-    implementation("org.apache.commons:commons-configuration2:2.8.0")
+    // https://mvnrepository.com/artifact/org.apache.commons/commons-configuration2
+    implementation("org.apache.commons:commons-configuration2:2.9.0")
+    // https://mvnrepository.com/artifact/commons-beanutils/commons-beanutils
     implementation("commons-beanutils:commons-beanutils:1.9.4")
     implementation("org.tinylog:tinylog-api-kotlin:2.6.2")
     implementation("org.tinylog:tinylog-impl:2.6.2")
     implementation("de.ovgu:featureide.lib.fm:3.7.2")
-    implementation("org.eclipse.jgit:org.eclipse.jgit:6.5.0.202303070854-r")
+    // https://mvnrepository.com/artifact/org.eclipse.jgit/org.eclipse.jgit
+    implementation("org.eclipse.jgit:org.eclipse.jgit:6.8.0.202311291450-r")
     implementation("org.sat4j:core:2.3.5")
     implementation("org.jetbrains:annotations:24.0.1")
     implementation("net.ssehub:kernel_haven:1.0.0")
