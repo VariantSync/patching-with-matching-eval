1263569030ee78185e35cbb0c31e0809b0d3fe9f
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2024-02-15 08:35:05 +0100

    fix(Docker): handle sudo execution of build script

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-docker build --network=host --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) -t sync-study .
+docker build --network=host --build-arg USER_ID=$(id -u ${SUDO_USER:-$USER}) --build-arg GROUP_ID=$(id -g ${SUDO_USER:-$USER}) -t extraction .
