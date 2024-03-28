6e63b282d48d6735750f917bf4bfa0e9d6290f82
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2024-03-28 16:21:23 +0100

    feat: add simple utility for counting the total number of cherry picks in our dataset

---
+++ src/main/kotlin/org/variantsync/evaluation/prstudy/CalcDatasetSize.kt
@@ -0,0 +1,33 @@
+package org.variantsync.evaluation.prstudy
+
+import org.tinylog.kotlin.Logger
+import org.variantsync.evaluation.EvalConfig
+import java.io.File
+import java.io.IOException
+import java.io.UncheckedIOException
+
+fun main(args: Array<String>) {
+    if (args.isEmpty()) {
+        System.err.println(
+            "The first argument should provide the path to the configuration file that is to be used"
+        )
+    }
+    val config = EvalConfig(File(args[0]))
+    Logger.info("Loading datasets")
+    val datasets: List<CherryDataset> = try {
+        loadPRDatasets(config.EXPERIMENT_DATASETS())
+    } catch (e: IOException) {
+        Logger.error(
+            "Was not able to load the yaml datasets from '"
+                    + config.EXPERIMENT_DATASETS() + "'"
+        )
+        throw UncheckedIOException(e)
+    }
+
+    var totalNumberOfCherryPicks = 0
+    for (dataset in datasets) {
+        totalNumberOfCherryPicks += dataset.cherryPicks.size
+    }
+
+    Logger.info("There are $totalNumberOfCherryPicks cherry picks to work on.")
+}
