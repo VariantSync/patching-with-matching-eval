becfdd408e3cdaa9794da95a56b7853abd5fa0d3
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2024-07-08 09:15:35 +0200

    feat(analysis): switch to poetry

--- src/main/python/README.md
+++ src/main/python/README.md
@@ -2,9 +2,9 @@
 Collection of python modules for analyzing the result data of our empirical evaluation. 
 
 ## Setup 
-Under linux, call the setup script to setup a virtual environment, activate it, and download the requirements:
-```shell 
-./setup.sh 
-```
+The project uses [poetry](https://python-poetry.org/) for dependency management.
 
-> To deactivate the environment, you can simply execute `deactivate` in the activated terminal. 
+Setup the environment and download dependencies with `poetry install`. 
+
+Then, activate the environment with `petry shell`.
+
--- src/main/python/setup.sh
+++ src/main/python/setup.sh
@@ -2,18 +2,13 @@
 
 ENV_NAME=".env"
 
-# Command to initialize local python environment and install requirements
-init_env() {
-    python3 -m venv $ENV_NAME
+python3 -m venv $ENV_NAME
 
-    echo "activate environment"
-    source $ENV_NAME/bin/activate
+echo "activate environment"
+source $ENV_NAME/bin/activate
 
-    if [ -f "result_analysis/requirements.txt" ]; then
-        pip install -r result_analysis/requirements.txt
-    else
-        echo "requirements.txt not found!"
-    fi
-}
-
-init_env
+if [ -f "result_analysis/requirements.txt" ]; then
+    pip install -r result_analysis/requirements.txt
+else
+    echo "requirements.txt not found!"
+fi
