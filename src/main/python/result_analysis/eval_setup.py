c8530cd21cd3f8397ca883af4ee40bbb8519683f
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2024-07-15 14:59:11 +0200

    chore(analysis): comment out unused patchers

--- src/main/python/result_analysis/eval_setup.py
+++ src/main/python/result_analysis/eval_setup.py
@@ -2,13 +2,13 @@ from enum import Enum
 
 
 class Patcher(Enum):
-    MPatch0 = "mpatch-0"
+    # MPatch0 = "mpatch-0"
     MPatch1 = "mpatch-1"
     MPatch2 = "mpatch-2"
     MPatch3 = "mpatch-3"
     UnixPatch = "unix_patch"
     GitApply = "git_apply"
-    GitCherryDefault = "git_cherry"
+    # GitCherryDefault = "git_cherry"
     GitCherryOurs = "git_cherry_ours"
     GitCherryTheirs = "git_cherry_theirs"
 
@@ -20,13 +20,13 @@ class Patcher(Enum):
 
     def nice_name(self):
         return {
-            Patcher.MPatch0: "PwM-0",
+            # Patcher.MPatch0: "PwM-0",
             Patcher.MPatch1: "PwM-1",
             Patcher.MPatch2: "PwM-2",
             Patcher.MPatch3: "PwM-3",
             Patcher.UnixPatch: "Unix Patch",
             Patcher.GitApply: "Git Apply",
-            Patcher.GitCherryDefault: "Git Cherry-Pick (default)",
+            # Patcher.GitCherryDefault: "Git Cherry-Pick (default)",
             Patcher.GitCherryOurs: "Git Cherry-Pick (ours)",
             Patcher.GitCherryTheirs: "Git Cherry-Pick (theirs)",
         }[self]
