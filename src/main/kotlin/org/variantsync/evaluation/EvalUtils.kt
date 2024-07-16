e91288094a3b1216968cf7d9c84861dec05d1d1e
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2024-07-16 08:28:04 +0200

    chore: add second PwM config

--- src/main/kotlin/org/variantsync/evaluation/EvalUtils.kt
+++ src/main/kotlin/org/variantsync/evaluation/EvalUtils.kt
@@ -31,7 +31,8 @@ fun filterUnpatchedFiles(originalPatch: OriginalDiff, diffToFilter: OriginalDiff
 fun defaultPatchers(strip: Int): List<Patcher> {
     val patchers = ArrayList<Patcher>()
     patchers.add(UnixPatch("unix_patch", strip))
-    patchers.add(MPatch("pwm", strip, 2))
+    patchers.add(MPatch("pwm_f1", strip, 1))
+    patchers.add(MPatch("pwm_f2", strip, 2))
     patchers.add(GitApply("git_apply", strip))
     patchers.add(GitCP("git_cherry", strip, MergeStrategy.Ours))
     return patchers
