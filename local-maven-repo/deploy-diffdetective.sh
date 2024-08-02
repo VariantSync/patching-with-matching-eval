114da41afb4f83a9d7609ea37da94f54583ed6e9
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2024-08-02 23:05:52 +0200

    clean local maven repo

--- local-maven-repo/deploy-diffdetective.sh
+++
@@ -1,2 +0,0 @@
-mvn deploy:deploy-file -DgroupId=org.anon -DartifactId=diffd -Dversion=1.0.0 -Durl=file:../local-maven-repo/ -DrepositoryId=local-maven-repo -DupdateReleaseInfo=true -Dfile=../src/main/resources/lib/diffd-1.0.0-jar-with-dependencies.jar
-rm -rf ~/.m2/repository/org/anon/
