24ddd8be235b25b4ff328a61d113a72e0cf48442
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2025-05-27 10:53:56 +0200

    feat: more precise composition analysis

--- src/main/kotlin/org/variantsync/evaluation/analysis/CompositionPrinting.kt
+++ src/main/kotlin/org/variantsync/evaluation/analysis/CompositionPrinting.kt
@@ -8,6 +8,8 @@ import java.util.*
 import kotlin.collections.HashMap
 import kotlin.system.exitProcess
 
+val n = 100
+
 fun main(args: Array<String>) {
     if (args.isEmpty()) {
         System.err.println(
@@ -24,8 +26,8 @@ fun main(args: Array<String>) {
     for (compositionResult in compositionResults) {
         val lang = compositionResult.evalRun.datasetName.split("_").first()
         for (entry in compositionResult.fileMap) {
-            val key = lang + "-" + entry.key
-            mergedResults[key] = mergedResults.getOrDefault(key, 0) + 1
+            val key = entry.key
+            mergedResults[key] = mergedResults.getOrDefault(key, 0) + entry.value
         }
     }
    // val mergedResults = mergeMaps(compositionResults.map { r -> r.fileMap })
@@ -34,13 +36,14 @@ fun main(args: Array<String>) {
     val numberFormatter = NumberFormat.getNumberInstance(Locale.US)
     val percentageFormatter = NumberFormat.getPercentInstance(Locale.US)
 
-    val top10 = topNValues(mergedResults, 10)
+    val topN = topNValues(mergedResults, n)
+    println("Number of file types: " + mergedResults.size)
     println("++++++++++++++++++++++++++++++++")
     println(" There are " + numberFormatter.format(mergedResults.values.sum()) + " patched files")
-    println("+++++ TOP 10 PATCHED FILES +++++")
-    val maxKeyLength = top10.maxOf { it.first.length }
-    val maxValLength = top10.maxOf { numberFormatter.format(it.second).length }
-    for (t in top10) {
+    println("+++++ TOP $n PATCHED FILES +++++")
+    val maxKeyLength = topN.maxOf { it.first.length }
+    val maxValLength = topN.maxOf { numberFormatter.format(it.second).length }
+    for (t in topN) {
         val percentage =  (t.second.toDouble() / mergedResults.values.sum().toDouble())
     println("${t.first.padEnd(maxKeyLength)} : ${numberFormatter.format(t.second).padStart(maxValLength)}   ${percentageFormatter.format(percentage)}")
     }
@@ -48,22 +51,25 @@ fun main(args: Array<String>) {
     println()
 
     val top10pure = HashMap<String, Int>()
-    for (t in top10) {
+    for (t in topN) {
         top10pure[t.first] = 0
     }
 
+    var impurePatches = 0
     for (r in compositionResults) {
-        val lang = r.evalRun.datasetName.split("_").first()
         if (r.fileMap.size == 1) {
-            val key = lang + "-" + r.fileMap.keys.first()
+            val key = r.fileMap.keys.first()
             if (top10pure.containsKey(key)) {
                 top10pure[key] = top10pure[key]!! + 1
             }
+        } else {
+            impurePatches++
         }
     }
 
     println(" There are " + numberFormatter.format(compositionResults.size) + " patches")
-    println("+++++ TOP 10 PURE PATCHES +++++")
+    println(" There are $impurePatches impure patches")
+    println("+++++ TOP $n PURE PATCHES +++++")
     for (pure in top10pure.entries
         .sortedByDescending { it.value }) {
         val percentage =  pure.value.toDouble() / compositionResults.size.toDouble()
