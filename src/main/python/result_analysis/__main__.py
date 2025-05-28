c8a67f602358a111abb38b63fa81ec69d97f80b6
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2025-05-28 12:41:53 +0200

    fix: update path to metrics table

--- src/main/python/result_analysis/__main__.py
+++ src/main/python/result_analysis/__main__.py
@@ -9,10 +9,7 @@ from result_analysis.analyze_results import find_outliers
 
 results_dir = "../../../evaluation-workdir/results/"
 repo_sample = "../../../evaluation-workdir/data/repo-sample.yaml"
-# metrics_file = "../../../evaluation-workdir/tables/metrics.tex"
-metrics_file = (
-    "/home/alex/papers/self/finished/patching-with-matching/paper/tables/metrics.tex"
-)
+metrics_file = "../../../evaluation-workdir/tables/metrics.tex"
 
 
 def main():
