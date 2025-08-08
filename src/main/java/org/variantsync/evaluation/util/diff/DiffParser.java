57b69e92a3d820e2172f2638b23810e39818fd39
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2025-08-08 15:43:46 +0200

    fix: handle SQL escapes

--- src/main/java/org/variantsync/evaluation/util/diff/DiffParser.java
+++ src/main/java/org/variantsync/evaluation/util/diff/DiffParser.java
@@ -81,22 +81,6 @@ public class DiffParser {
                         fileDiffContent = new ArrayList<>();
                     }
                 }
-            } else if (line.contains(fileDiffStart)) {
-                if (indexNext < lines.size()) {
-                    final String nextLine = lines.get(indexNext);
-                    if (nextLine.startsWith(fileDiffFollow)) {
-                        final String additionalContent = line.substring(0, line.indexOf(fileDiffStart));
-                        // Create a FileDiff from the collected lines
-                        if (fileDiffContent != null) {
-                            fileDiffContent.add(additionalContent);
-                            fileDiffs.add(parseFileDiff(fileDiffContent));
-                        }
-                        // Reset the lines that should go into the next FileDiff
-                        fileDiffContent = new ArrayList<>();
-                        fileDiffContent.add(line.substring(line.indexOf(fileDiffStart)));
-                        continue;
-                    }
-                }
             }
             if (fileDiffContent == null) {
                 throw new IllegalArgumentException("The provided lines do not contain one of the expected fileDiffStart values");
