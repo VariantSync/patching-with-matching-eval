9fc71805749d8b8e18a291ec2e5d66c3526c4ad1
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2025-08-08 14:49:57 +0200

    Revert "feat: cleaning of repos without cherry picks"
    This reverts commit 69b9952c9b5a771d008cf2a481cb5051fd09c29a.

--- src/main/kotlin/org/variantsync/evaluation/PatcherEvaluationMain.kt
+++ src/main/kotlin/org/variantsync/evaluation/PatcherEvaluationMain.kt
@@ -1,12 +1,26 @@
 package org.variantsync.evaluation
 
+import org.apache.commons.io.FileUtils
+import org.eclipse.jgit.api.Git
+import org.tinylog.kotlin.Logger
+import org.variantsync.evaluation.execution.*
+import org.variantsync.evaluation.util.shell.CpCommand
+import org.variantsync.evaluation.util.shell.RmCommand
+import org.variantsync.evaluation.util.shell.ShellExecutor
+import org.yaml.snakeyaml.LoaderOptions
+import org.yaml.snakeyaml.Yaml
 import java.io.File
 import java.io.IOException
 import java.io.UncheckedIOException
+import java.math.RoundingMode
 import java.nio.ByteBuffer
+import java.nio.file.FileVisitResult
 import java.nio.file.Files
 import java.nio.file.Path
+import java.nio.file.SimpleFileVisitor
+import java.nio.file.attribute.BasicFileAttributes
 import java.security.SecureRandom
+import java.text.DecimalFormat
 import java.util.*
 import java.util.concurrent.BlockingQueue
 import java.util.concurrent.Executors
@@ -19,25 +33,22 @@ import kotlin.collections.HashSet
 import kotlin.math.max
 import kotlin.math.min
 import kotlin.system.exitProcess
-import org.apache.commons.io.FileUtils
-import org.tinylog.kotlin.Logger
-import org.variantsync.evaluation.execution.*
-import org.variantsync.evaluation.util.shell.RmCommand
-import org.variantsync.evaluation.util.shell.ShellExecutor
 
 class CherryPickStudy(
-        val config: EvalConfig,
-        dataset: CherryDataset,
-        repetition: Int,
-        idProvider: IDProvider,
-        completedRuns: Set<EvaluationRun>,
+    val config: EvalConfig,
+    dataset: CherryDataset,
+    repetition: Int,
+    idProvider: IDProvider,
+    completedRuns: Set<EvaluationRun>,
 ) {
     // The study tasks that are to be executed in parallel
     private val evalTasks: MutableList<CherryPickEvalTask>
     private val numThreads: Int
     private val availableOperations: BlockingQueue<EvalOperations>
 
-    /** Initialize the study from the given configuration */
+    /**
+     * Initialize the study from the given configuration
+     */
     init {
         if (!Files.exists(config.EXPERIMENT_DIR_RESULTS())) {
             Files.createDirectories(config.EXPERIMENT_DIR_RESULTS())
@@ -49,8 +60,7 @@ class CherryPickStudy(
 
         Logger.info("Preparing working directories for $numThreads threads.")
         for (i in 1..numThreads) {
-            // Add one operations instance for each thread; each instance defines its own working
-            // directory
+            // Add one operations instance for each thread; each instance defines its own working directory
             val operations = EvalOperations(config.EXPERIMENT_DIR_MAIN(), repoPath)
             // Clean old variant files
             cleanVariantDirectories(operations)
@@ -63,52 +73,42 @@ class CherryPickStudy(
 
         for (cherryPick in dataset.cherryPicks) {
             val runID = idProvider.next()
-            val run =
-                    EvaluationRun(
-                            repetition,
-                            dataset.datasetName,
-                            cherryPick.cherryCommit,
-                            cherryPick.expectedResultCommit
-                    )
+            val run = EvaluationRun(repetition, dataset.datasetName, cherryPick.cherryCommit, cherryPick.expectedResultCommit)
             if (completedRuns.contains(run)) {
                 Logger.info("Skipped cherry pick of run $runID (already processed)")
                 continue
             }
             evalTasks.add(
-                    CherryPickEvalTask(
-                            repetition,
-                            config,
-                            dataset.datasetName,
-                            cherryPick,
-                            availableOperations,
-                            runID,
-                            run,
-                    )
+                CherryPickEvalTask(
+                    repetition,
+                    config,
+                    dataset.datasetName,
+                    cherryPick,
+                    availableOperations,
+                    runID,
+                    run,
+                )
             )
         }
     }
 
-    /** Execute the study. */
+    /**
+     * Execute the study.
+     */
     fun run() {
         val threadPool = Executors.newFixedThreadPool(numThreads)
         Logger.info("Scheduling ${evalTasks.size} tasks...")
 
-        val futures =
-                evalTasks
-                        .stream()
-                        .map { runnable: CherryPickEvalTask ->
-                            FutureAndEvalRun(threadPool.submit(runnable), runnable.evalRun)
-                        }
-                        .collect(Collectors.toList())
+        val futures = evalTasks.stream()
+            .map { runnable: CherryPickEvalTask -> FutureAndEvalRun(threadPool.submit(runnable), runnable.evalRun) }
+            .collect(Collectors.toList())
 
         Logger.info("Scheduled all tasks.")
 
         val hadTimeout = waitForShutdown(threadPool, futures, config)
 
         if (hadTimeout) {
-            Logger.info(
-                    "Timeout detected. Marking task of ${evalTasks.first().evalRun.datasetName} as completed."
-            )
+            Logger.info("Timeout detected. Marking task of ${evalTasks.first().evalRun.datasetName} as completed.")
             for (evalTask in evalTasks) {
                 markEvalRun(evalTask.evalRun, config.EXPERIMENT_PROCESSED_FILE())
             }
@@ -124,9 +124,7 @@ class CherryPickStudy(
                 if (Files.exists(operations.workDir)) {
                     Logger.debug("Trying to remove directory with 'rm -rf'")
                     if (ShellExecutor(Logger::warn, Logger::warn, operations.workDir)
-                                    .execute(RmCommand(operations.workDir).recursive().force())
-                                    .isSuccess
-                    ) {
+                        .execute(RmCommand(operations.workDir).recursive().force()).isSuccess) {
                         Logger.debug("Success!")
                     }
                 }
@@ -136,37 +134,33 @@ class CherryPickStudy(
     }
 }
 
+
 fun main(args: Array<String>) {
     if (args.isEmpty()) {
         System.err.println(
-                "The first argument should provide the path to the configuration file that is to be used"
+            "The first argument should provide the path to the configuration file that is to be used"
         )
     }
     val config = EvalConfig(File(args[0]))
     Logger.info("Starting experiment initialization.")
-    val datasetsPerLanguage: Map<String, MutableList<CherryDataset>> =
-            try {
-                loadPRDatasets(config)
-            } catch (e: IOException) {
-                Logger.error(
-                        "Was not able to load the yaml datasets from '" +
-                                config.EXPERIMENT_DATASETS() +
-                                "'"
-                )
-                throw UncheckedIOException(e)
-            }
+    val datasetsPerLanguage: Map<String, MutableList<CherryDataset>> = try {
+        loadPRDatasets(config)
+    } catch (e: IOException) {
+        Logger.error(
+            "Was not able to load the yaml datasets from '"
+                    + config.EXPERIMENT_DATASETS() + "'"
+        )
+        throw UncheckedIOException(e)
+    }
 
-    val seed: ByteArray =
-            ByteBuffer.allocate(java.lang.Long.BYTES)
-                    .putLong(config.EXPERIMENT_REPEATS_START() + config.SEED())
-                    .array()
+    val seed: ByteArray = ByteBuffer.allocate(java.lang.Long.BYTES).putLong(config.EXPERIMENT_REPEATS_START()
+            + config.SEED()).array()
     val idProvider = IDProvider(config.EXPERIMENT_START_ID())
     var id = 0uL
     val rand = SecureRandom(seed)
     val allSamples = createOrLoadSamples(config, datasetsPerLanguage, rand)
 
     cloneDatasets(allSamples, config)
-    cleanUnrequiredDatasets(allSamples, config)
 
     val n = 5
     Logger.info("Processing $n repos in parallel")
@@ -181,38 +175,26 @@ fun main(args: Array<String>) {
         val completedRuns = completedRunsAll.getOrDefault(repetition, HashMap())
         var completed = 0
         Logger.info("Already considered ${completedRuns.size} repos.")
-        completedRuns.forEach { s -> completed += s.value.size }
+        completedRuns.forEach { s -> completed += s.value.size}
         Logger.info("Processed a total of $completed evaluation runs.\n")
         Thread.sleep(5000)
 
-        Logger.info(
-                "Considering a total of $numCherryPicks cherry-picks for repetition $repetition"
-        )
+
+        Logger.info("Considering a total of $numCherryPicks cherry-picks for repetition $repetition")
         for (dataset in allSamples[repetitionIndex]) {
             while (idProvider.next() < id) {}
             id += dataset.cherryPicks.size.toUInt()
 
-            if (completedRuns.contains(dataset.datasetName) &&
-                            completedRuns[dataset.datasetName]!!.size == dataset.cherryPicks.size
-            ) {
+            if (completedRuns.contains(dataset.datasetName) && completedRuns[dataset.datasetName]!!.size == dataset.cherryPicks.size) {
                 // Skip this dataset, it was already processed
-                Logger.info(
-                        "Skipping evaluation of cherry picks from ${dataset.datasetName} (rep.: $repetition)"
-                )
+                Logger.info("Skipping evaluation of cherry picks from ${dataset.datasetName} (rep.: $repetition)")
                 printProgress(completed, numCherryPicks, repetition, 0uL)
                 continue
             }
             threadPool.submit {
                 val i = id
                 Logger.info("Preparing evaluation of cherry picks from ${dataset.datasetName}")
-                val study =
-                        CherryPickStudy(
-                                config,
-                                dataset,
-                                repetition,
-                                idProvider,
-                                completedRuns.getOrDefault(dataset.datasetName, HashSet())
-                        )
+                val study = CherryPickStudy(config, dataset, repetition, idProvider, completedRuns.getOrDefault(dataset.datasetName, HashSet()))
                 try {
                     study.run()
                 } catch (e: Exception) {
@@ -229,3 +211,4 @@ fun main(args: Array<String>) {
 
     exitProcess(0)
 }
+
